3a77fa7361c63bc94fdd054b678db6d7a72aabf7
Ken <ken.farmer@protectwise.com>
2015-02-21 14:42:03 -0700

    Added pathing to make it easier for tests to find the external scripts to run

--- cletus/tests/test_cletus_job.py
+++ cletus/tests/test_cletus_job.py
@@ -16,13 +16,12 @@ import shutil
 import envoy
 import pytest
 import fcntl
+from os.path import dirname, basename, isfile, isdir, exists
 
-#sys.path.insert(1, '../')
-#import cletus_job as  mod
-
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+sys.path.insert(0, dirname(dirname(dirname(os.path.abspath(__file__)))))
 import cletus.cletus_job  as mod
 
+test_path = dirname(os.path.realpath((__file__)))
 
 
 
@@ -81,23 +80,23 @@ class TestCompetingJobs(object):
 
        # confirm it locks & responds right in the
        # simplest case.
-       cmd1     = '''run_cletus_job_once.py      \
+       cmd1     = '''%s/run_cletus_job_once.py     \
                        --lock-wait 1               \
                        --post-lock-sleep 1         \
-                  '''
+                  ''' % test_path
        self.c1   = envoy.run(cmd1)
        assert self.c1.status_code == 0 # locked
-       
+
 
     def test_single_asynch_process(self):
        """ Objective is to confirm that this method of 
            running the job will work correctly.
        """
 
-       cmd1     = '''run_cletus_job_once.py      \
+       cmd1     = '''%s/run_cletus_job_once.py     \
                        --lock-wait 1               \
                        --post-lock-sleep 1         \
-                  '''
+                  ''' % test_path
        #--------------------------------------------
        # note that due to a bug in envoy, you must 
        # block before you can check the status_code
@@ -105,23 +104,25 @@ class TestCompetingJobs(object):
        self.c   = envoy.connect(cmd1)
        self.c.block() 
        assert self.c.status_code == 0 # locked
-       
+
 
     def test_two_asynch_running(self):
 
        #---- get lock & hold it
-       cmd1     = '''run_cletus_job_once.py      \
-                       --lock-wait  0              \
-                       --post-lock-sleep 3       '''
+       cmd1     = '''%s/run_cletus_job_once.py  \
+                       --lock-wait  0           \
+                       --post-lock-sleep 3      \
+                  ''' % test_path
        self.c   = envoy.connect(cmd1)
 
        #---- ensure cmd1 locks file before cmd2 starts!
-       time.sleep(0.5)  
+       time.sleep(0.5)
 
        #---- try to get lock, fail, quit fast 
-       cmd2     = '''run_cletus_job_once.py      \
-                       --lock-wait  0              \
-                       --post-lock-sleep 0       '''
+       cmd2     = '''%s/run_cletus_job_once.py   \
+                       --lock-wait  0            \
+                       --post-lock-sleep 0       \
+                  ''' % test_path
        self.c2  = envoy.connect(cmd2)
 
        #---- finish cmd2, then finish cmd1
@@ -135,18 +136,22 @@ class TestCompetingJobs(object):
     def test_two_asynch_running_retry_locks(self):
 
        #--- get lock & hold it
-       cmd1     = '''run_cletus_job_once.py      \
-                       --lock-wait  0              \
-                       --post-lock-sleep 2       '''
+       cmd1     = '''%s/run_cletus_job_once.py   \
+                       --lock-wait  0            \
+                       --post-lock-sleep 2       \
+                  ''' % test_path
+       print(cmd1)
        self.c   = envoy.connect(cmd1)
 
        #---- ensure cmd1 locks file before cmd2 starts!
-       time.sleep(0.5)  
+       time.sleep(0.5)
 
        #---- try to get lock, wait for it, get it
-       cmd2     = '''run_cletus_job_once.py      \
-                       --lock-wait  3              \
-                       --post-lock-sleep 0       '''
+       cmd2     = '''%s/run_cletus_job_once.py \
+                       --lock-wait  3         \
+                       --post-lock-sleep 0    \
+                  ''' % test_path
+       print(cmd2)
 
        self.c2  = envoy.connect(cmd2)
 
