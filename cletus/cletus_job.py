e4d35d8b60891785004f68ec48b95f828546f9db
Ken <ken.farmer@protectwise.com>
2015-05-19 12:04:37 -0600

    futured

--- cletus/cletus_job.py
+++ cletus/cletus_job.py
@@ -16,6 +16,7 @@
     See the file "LICENSE" for the full license governing use of this file.
     Copyright 2013, 2014 Ken Farmer
 """
+from __future__ import absolute_import
 
 
 import os
@@ -116,7 +117,7 @@ class JobCheck(object):
         """
         try:
             self.pidfd = open(pid_fqfn, 'a')
-        except IOError, e:
+        except IOError as e:
             self.logger.critical('Could not open pidfile: %s - permissions? missing dir?' % e)
             raise
         else:
@@ -187,7 +188,7 @@ class JobCheck(object):
             else:
                 err_msg = 'app_name must be provided if pid_dir is not'
                 self.logger.critical(err_msg)
-                raise ValueError, err_msg
+                raise ValueError(err_msg)
 
         # next try to create it, just in case it isn't there
         try:
